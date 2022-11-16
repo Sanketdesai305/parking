5c46e0e19897aca342e9d605f30b41de453f2500
unknown <desai.sanket92@gmail.com>
2022-11-16 14:11:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 function App() {
+
+  const [user,setUser]= useState("")
+  const [num,setNum]= useState("")
+  const [hour,setHour]= useState("")
+  const [time,settime]= useState("")
+  const [exit,setexit]= useState({})
+  const [todo,settodo]= useState([])
+
+  const clickHandler=(e)=>{
+    e.preventDefault();
+    settodo([...todo,{id:uuidv4(),Name:user,Number:num,Enter:time,Exit:exit}])
+    const date = new Date();
+    const Ex = new Date();
+    const exd = new Date(Ex.getTime() + (hour*60) * 60 * 1000)
+    setexit(exd.getHours()+":"+date.getMinutes()+":"+date.getSeconds())
+    const tid = (date.getHours()+":"+date.getMinutes()+":"+date.getSeconds())
+    settime(tid)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='form'>
+        <input placeholder='Driver Name' onChange={(e)=>setUser(e.target.value)}/>
+        <input placeholder='Vehicle Number' onChange={(e)=>setNum(e.target.value)}/>
+        <input placeholder='How many Hours' onChange={(e)=>setHour(e.target.value)}/>
+        <button className='button' onClick={clickHandler}>Submit</button>
+      </div>
+        {todo.filter((f)=>f.Enter).map((to)=>(
+          <ul className='list' key={to.id}>
+            <li>{to.Name}</li>
+            <li>{to.Number}</li>
+            <li>{to.Enter}</li>
+            <li>{JSON.stringify(to.Exit)}</li>
+          </ul>
+        ))}
     </div>
   );
 }
